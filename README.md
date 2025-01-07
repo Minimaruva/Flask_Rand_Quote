392de13e70e061b7f9e5a2e6a80fa36ee2d3678c
Vlad Y <vladjagnion@gmail.com>
2025-01-07 23:15:27 +0000

    working quote get from csv file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 An example project to learn how to build a simple Flask API
+
+Author of quotes csv: https://gist.github.com/JakubPetriska/060958fd744ca34f099e947cd080b540
--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
 import random
+import pandas as pd
+
 app = Flask(__name__)
 
-
-
 # Quotes data
-quotes = [
-    {"quote": "Life is what happens when you're busy making other plans.", "author": "John Lennon"},
-    {"quote": "The purpose of our lives is to be happy.", "author": "Dalai Lama"},
-    {"quote": "Get busy living or get busy dying.", "author": "Stephen King"}
-]
+quotes_df = pd.read_csv("./assets/quotes.csv")
+quotes_num = len(quotes_df)
 
+
+@app.route('/')
+def hello():
+    greet = '<h1>This is a random quote generator!</h1>'
+    link = '<p><a href="/get_quote">Get quote!</a></p>'
+    return greet + link
+
+# This decorator sets the path for the function below like http.../get_quote
 @app.route('/get_quote')
 def get_quote():
-    quote = random.choice(quotes)
-    return f'<h1> {quote["quote"]} </h1>'
+    random_row = random.randint(1, quotes_num)
+    quote = quotes_df.loc[random_row]
+    link = '<br><p><a href="/get_quote">Get new quote!</a></p>'
+    return f'<h1> {quote["Quote"]} </h1><br><p>{quote["Author"]}<p>' + link
+
+
+
+if __name__ == '__main__':
+    # Run the app in debug mode for live updates
+    app.run(debug=True)
